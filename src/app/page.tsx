922a019560411bc9d62a53c3a027180a5bb2a563
Bruno Mathias <bm.grey@outlook.com>
2024-01-24 23:28:15 -0300

    Final Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Chat } from "@/components/chat";
 
-
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-slate-50">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900">
       <Chat />
+      <span className="text-sm text-slate-400 mt-3">
+        Developed by Bruno Mathias using the GPT-3 API and Vercel SDK.💖
+      </span>
     </div>
   );
 }
--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -12,47 +12,54 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { useChat } from "ai/react";
+import { ScrollArea } from "./ui/scroll-area";
 
 export interface ChatProps {}
 
 export function Chat(props: ChatProps) {
   const { messages, input, handleInputChange, handleSubmit } = useChat({
-    api: '/api/chat'
+    api: "/api/chat",
   });
 
   return (
-    <Card className="w-[500px] h-[700px] grid grid-rows-[min-content_1fr_min-content]">
+    <Card className="w-[500px]">
       <CardHeader>
-        <CardTitle>Chat AI</CardTitle>
-        <CardDescription>Using Vercel SDK to create a chat bot</CardDescription>
+        <CardTitle>Bruno Mathias GPT Chat AI</CardTitle>
+        <CardDescription>
+          Ask me anything that comes to your mind.
+        </CardDescription>
       </CardHeader>
-      <CardContent className="space-y-4">
-        {messages.map((message) => {
-          return (
-            <div key={message.id} className="flex gap-2 text-gray-700 text-sm">
-              {message.role === "user" && (
-                <Avatar>
-                  <AvatarFallback>AI BM</AvatarFallback>
-                  <AvatarImage src="https://github.com/ThiasGrey.png" />
-                </Avatar>
-              )}
-              {message.role === 'assistant' && (
-                <Avatar>
-                  <AvatarFallback>AI BM</AvatarFallback>
-                  <AvatarImage src="https://github.com/RocketSeat.png" />
-                </Avatar>
-              )}
+      <CardContent>
+        <ScrollArea className="h-[640px] w-full space-y-4 px-4">
+          {messages.map((message) => {
+            return (
+              <div
+                key={message.id}
+                className="flex gap-2 text-stone-900 text-sm mt-4"
+              >
+                {message.role === "user" && (
+                  <Avatar>
+                    <AvatarFallback>AI User</AvatarFallback>
+                    <AvatarImage src="https://github.com/OpenUserJS.png" />
+                  </Avatar>
+                )}
+                {message.role === "assistant" && (
+                  <Avatar className="ml-5">
+                    <AvatarFallback>AI Assistent</AvatarFallback>
+                    <AvatarImage src="https://github.com/ThiasGrey.png" />
+                  </Avatar>
+                )}
 
-
-              <p className="leading-relaxed">
-                <span className="block font-bold text-slate-800">
-                  {message.role === "user" ? "User" : "AI"}
-                </span>
-                {message.content}
-              </p>
-            </div>
-          );
-        })}
+                <p className="leading-relaxed ">
+                  <span className="block font-bold text-slate-800">
+                    {message.role === "user" ? "User" : "Bruno Mathias IA"}
+                  </span>
+                  {message.content}
+                </p>
+              </div>
+            );
+          })}
+        </ScrollArea>
       </CardContent>
       <CardFooter>
         <form className="gap-2 flex w-full" onSubmit={handleSubmit}>
@@ -63,7 +70,9 @@ export function Chat(props: ChatProps) {
           />
           <Button type="submit">Send</Button>
         </form>
+       
       </CardFooter>
     </Card>
+    
   );
 }
